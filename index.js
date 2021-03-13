c70deb187776cefe2b5bf7176312ce0b4f11f620
EMIKEI <72548507+EMIKEI@users.noreply.github.com>
2021-03-13 15:06:57 +0900

    song title 보여줌, song을 드라이브에서 불러옴

--- index.js
+++ index.js
@@ -1,105 +1,83 @@
-const playBut = document.querySelector(".playBut")
-const playButImg = document.querySelector(".playButImg")
-const locationBar = document.querySelector(".locationBar")
-const locationBarZone = document.querySelector(".locationBarZone")
-const locationBarZoneWidth = locationBarZone.offsetWidth;
-const songs = [
-    new Audio('test.mp3'), new Audio('test2.mp3')
-]
+const loadingPage = document.querySelector(".loadingPage")
+const selectFoodZone = document.querySelector(".selectFoodZone");
+const foodButs = document.querySelector(".foodButs");
+const foodBut = document.querySelectorAll(".foodBut");
+const mainZone = document.querySelector(".mainZone");
+const foodImg = document.querySelector(".foodImg");
+const storeBut = document.querySelector(".storeBut")
 
-const pauseImg = "https://han.gl/NXpK5";
-const playImg = "https://han.gl/d8uDv";
+const scrollFoodZone = document.querySelectorAll(".scrollFoodZone");
+const selectFoodZoneWidth = selectFoodZone.offsetWidth;
 
-let locationBarZonescroll = null;
-let clickPlayButWait = null;
-let pauseTime = null;
-let randomSong = songs[Math.floor(Math.random()*songs.length +0)];
+const foodImgBackgroundImg = [
+    "url('https://han.gl/XhaeW')", 
+    "url('http://collectionpng.com/images/13732.png')",
+    "url('https://t2.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2Vuw/image/NKzQL4XkH0ugI222QJQAGFPKwfU.png')",
+];
 
-const songT = Math.floor(randomSong.currentTime);
+//lodingPage
+loadingPage.style.display="flex";
 
 window.addEventListener("load", function(){
-   setup();
+    setTimeout(function(){
+        loadingPage.style.display = "none";
+    },2000)
 })
 
-playBut.addEventListener("click", function(){
-    randomSongPlay();
-})
+// click foodBut
+for(let i = 0; i< foodBut.length; i++ ){
+    foodBut[i].addEventListener("click", function(){
 
-function setup(){
-    playBut.name = "start";
-    locationBarZone.scrollTo(locationBarZoneWidth, 0);
-    playButImg.src = playImg;
+        for(let i2 = 0; i2 < foodBut.length; i2++){
+            foodBut[i2].classList.remove('selectedBut')
+        }
+
+        foodBut[i].classList.add('selectedBut')
+        foodImg.style.backgroundImage = foodImgBackgroundImg[i]; 
+    })
 }
 
-function randomSongPlay(){
-    if(playBut.name === "start"){
-        randomSong.play();
-        playBut.name = "pause";
-        playButImg.src = pauseImg;
-        console.log(Math.round(randomSong.duration))
 
-        locationBarZonescroll=
+//scroll foodButs
+const foodButsWidth = foodButs.offsetWidth;
+
+// 왼쪽 scrollFoodZone
+scrollFoodZone[0].addEventListener("mouseenter", function(){
+    foodButs.scrollBy({
+        left: -foodButsWidth/4,
+        behavior:"smooth"
+    })
+    scrollfoodZone1=
+    setInterval(function(){
+        foodButs.scrollBy({
+            left: -foodButsWidth/4,
+            behavior:"smooth"
+        })
+    },470)
+})
+scrollFoodZone[0].addEventListener("mouseout",function(){
+    clearInterval(scrollfoodZone1)
+})
+//오른쪽 scrollFoodZone
+scrollFoodZone[1].addEventListener("mouseenter", function(){
+        foodButs.scrollBy({
+            left: foodButsWidth/4,
+            behavior:"smooth"
+        })
+        scrollfoodZone2=
         setInterval(function(){
-            locationBarZone.scrollBy({
-                left: -locationBarZoneWidth/randomSong.duration,
+            foodButs.scrollBy({
+                left: foodButsWidth/4,
                 behavior:"smooth"
             })
-        }, 1000)
+        },470)
+})
+scrollFoodZone[1].addEventListener("mouseout",function(){
+    clearInterval(scrollfoodZone2)
+})
 
-    }else if(playBut.name === "play"){
-        randomSong.play();
-        playBut.name = "pause";
-        playButImg.src = pauseImg;
+//storeBut
 
-        console.error(Number(Math.ceil(pauseTime)-pauseTime)*1000)
-
-        clickPlayButWait=
-        setTimeout(function(){
-            locationBarZone.scrollBy({
-                left: -locationBarZoneWidth/randomSong.duration,
-                behavior:"smooth"
-            })  
-            locationBarZonescroll=
-            setInterval(function(){
-                locationBarZone.scrollBy({
-                    left: -locationBarZoneWidth/randomSong.duration,
-                    behavior:"smooth"
-                })  
-            },1000)
-        },Number(Math.ceil(pauseTime)-pauseTime)*1000)
-        // pauseTime을 올림한 것에서 pauseTime을 빼고 1000을 곱한 후 그 값만큼 기다린다.
-    }else if(playBut.name === "pause"){
-        randomSong.pause();
-        playBut.name = "play";
-        playButImg.src = playImg;
-
-        pauseTime = randomSong.currentTime;
-        clearInterval(locationBarZonescroll)
-        clearTimeout(clickPlayButWait)
-        console.log(Math.ceil(pauseTime), pauseTime, playBut.name )
-    
-    }
-
-    randomSong.addEventListener("ended", function(){
-        playBut.name = "start";
-
-        console.log("ended");
-    
-        setTimeout(function(){
-          clearInterval(locationBarZonescroll)
-          playButImg.src = playImg;  
-          locationBarZone.scrollTo(locationBarZoneWidth,0)
-        },2000)
-        
-    })
-    
-}
-
-/* 
-문제점
- 
-음악의 제목을 보여줘야함,
-음악이 끝나면 어떻게 할 것인지 생각해야함
-
-
-*/
+storeBut.addEventListener("click", function(){
+    alert("준비 중")
+})
